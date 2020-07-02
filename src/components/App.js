6e86c0d4646dbdecda2ee95fc41ffbdc777c8afb
Vicente Blat <blat98v@gmail.com>
2020-07-02 18:52:27 -0400

    Create data.json file and state component

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,26 @@ import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      myName: 'Vicente'
-    }
+      myAppointments: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        });
+        this.setState({
+          myAppointments: apts
+        });
+      });
   }
 
 
@@ -21,7 +35,6 @@ class App extends Component {
         <div className="row">
           <div className="col-md-12 bg-white">
             <div className="container">
-              { this.state.myName }
               <AddAppointments />
               <SearchAppointments />
               <ListAppointments />
